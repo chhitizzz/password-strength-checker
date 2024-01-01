8de89c5a3f553fa71654d67e1ce0a377285a7b5a
ChhitizV <chhitizvaidya10@gmail.com>
2024-01-01 18:41:38 +0545

    in progress

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import string
 
 password = "helloworld"
 
-upper_case = [1 if c in string.ascii_uppercase else 0 for c in password]
-
-print(string.ascii_uppercase)
+upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password])
+lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password])
+special_characters = any([1 if c in string.punctuation else 0 for c in password])
+digits = any([1 if c in string.digits else 0 for c in password])
