fa391ae88f34f191f7fcb4f7f250a366922c3645
ChhitizV <chhitizvaidya10@gmail.com>
2024-01-02 21:18:55 +0545

    len

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password])
 lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password])
 special_characters = any([1 if c in string.punctuation else 0 for c in password])
 digits = any([1 if c in string.digits else 0 for c in password])
+
+characters = [upper_case, lower_case, special_characters, digits]
+
+length = len(password)
