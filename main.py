6b0cbbb999755774ca0c4d0fc7ffa6d92049cc72
ChhitizV <chhitizvaidya10@gmail.com>
2024-01-02 21:22:56 +0545

    score

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ digits = any([1 if c in string.digits else 0 for c in password])
 characters = [upper_case, lower_case, special_characters, digits]
 
 length = len(password)
+
+score = 0
+
+if length > 8:
+    score += 1
+if length > 12:
+    score += 1
+if length > 16:
+    score += 1
+if length > 20:
+    score += 1
